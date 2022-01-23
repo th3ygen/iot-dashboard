212a25b6cf7e1b125a39098bba115d1a2b980a34
Aidil <th3ygen@gmail.com>
2022-01-23 23:33:28 +0800

    before final presentation

--- react/src/page/user/filter/Filters.jsx
+++ react/src/page/user/filter/Filters.jsx
@@ -7,86 +7,114 @@ import Table from "components/Table.component";
 import styles from "styles/user/filter/Filters.module.scss";
 
 function FiltersPage() {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const [user, setUser] = useOutletContext();
+	const [user, setUser] = useOutletContext();
 
-    const [filters, setFilters] = useState([]);
-    
-    useEffect(() => {
-        (async () => {
-            let request, response;
+	const [filters, setFilters] = useState([]);
+    const [refresh, setRefresh] = useState(false);
 
-            request = await fetch('http://localhost:8080/api/filter/owned', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    auth: user.token,
-                },
-            });
+	useEffect(() => {
+        if (!user.token) {
+            return;
+        }
 
-            if (request.status === 200) {
-                response = await request.json();
+		(async () => {
+			let request, response;
 
-                console.log(response);
+            console.log('refresh');
 
-                
-                setFilters(
-                    response.map((filter) => {
-                        const date = new Date(filter.createdAt);
-                        const dateString = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}.${date.getMinutes()}`;
-                        
-                        return [
-                            filter._id,
-                            filter.label,
-                            filter.fields.length,
-                            dateString,
-                        ]
-                    })
-                );
-            }
-        })();
-    }, [user.token]);
+			request = await fetch("http://localhost:8080/api/filter/owned", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					auth: user.token,
+				},
+			});
 
-    return (
-        <div className={styles.container}>
-            <PageHeader
-                title="Filters"
-                brief="Manage your filters"
-                navs={[
-                    {
-                        name: "Add filter",
-                        path: "/user/filters/add",
-                        icon: "FaPlus",
-                    },
-                ]}
-            />
-            <div className={styles.content}>
-                <Table
-                    title="Manage filters"
-                    headers={["Filter", "Total fields", "Last update"]}
-                    items={filters}
-                    colWidthPercent={["30%", "10%", "10%"]}
-                    actions={[
-                        {
-                            tooltip: "Edit",
-                            icon: "FaEdit",
-                            callback: (id) => {
-                                navigate('/user/filters/edit/', { replace: true, state: { id } });
-                            },
-                        },
-                        {
-                            tooltip: "Delete",
-                            icon: "FaTrash",
-                            callback: async (id) => {
-                                console.log(id);
-                            },
-                        },
-                    ]}
-                ></Table>
-            </div>
-        </div>
-    );
+			if (request.status === 200) {
+				response = await request.json();
+
+				setFilters(
+					response.map((filter) => {
+						const date = new Date(filter.createdAt);
+						const dateString = `${date.getDate()}/${
+							date.getMonth() + 1
+						}/${date.getFullYear()} ${date.getHours()}.${date.getMinutes()}`;
+
+						return [
+							filter._id,
+							filter.label,
+							filter.fields.length,
+							dateString,
+						];
+					})
+				);
+			}
+		})();
+	}, [user.token, refresh]);
+
+	return (
+		<div className={styles.container}>
+			<PageHeader
+				title="Filters"
+				brief="Manage your filters"
+				navs={[
+					{
+						name: "Add filter",
+						path: "/user/filters/add",
+						icon: "FaPlus",
+					},
+				]}
+			/>
+			<div className={styles.content}>
+				<Table
+					title="Manage filters"
+					headers={["Filter", "Total fields", "Last update"]}
+					items={filters || []}
+					colWidthPercent={["30%", "10%", "10%"]}
+					actions={[
+						{
+							tooltip: "Edit",
+							icon: "FaEdit",
+							callback: (id) => {
+								navigate("/user/filters/edit/", {
+									replace: true,
+									state: { id },
+								});
+							},
+						},
+						{
+							tooltip: "Delete",
+							icon: "FaTrash",
+							callback: async (id) => {
+								try {
+									const req = await fetch(
+										"http://localhost:8080/api/filter/delete/" +
+											id,
+										{
+											method: "DELETE",
+											headers: {
+												"Content-Type":
+													"application/json",
+												auth: user.token,
+											},
+										}
+									);
+
+                                    if (req.status === 200) {
+                                        setRefresh(!refresh);
+                                    }
+								} catch (e) {
+									console.log(e);
+								}
+							},
+						},
+					]}
+				></Table>
+			</div>
+		</div>
+	);
 }
 
 export default FiltersPage;
